561d50e7246b0d61d570d947ea61f29e540c34e2
Serzsh <serzants.janis@gmail.com>
2023-02-14 15:00:33 +0200

    added comments

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
 class DatoraDetala {
     constructor(veids, modelis, cena) {
+        // validējam vai ir dati, ja nav dati tad metam erroru un tālāk nekas neizpildās
         this.validetDetalasDatus(veids, modelis, cena)
 
+        // izveidojam jaunu detaļu
         this.veids = veids
         this.modelis = modelis
         this.cena = cena
     }
 
     apskatitDetalu() {
+        // iegūstam detaļu
         return {
             veids: this.veids,
             modelis: this.modelis,
@@ -16,13 +19,16 @@ class DatoraDetala {
     }
 
     labotDetalu (veids, modelis, cena) {
+        // validējam vai ir dati, ja nav dati tad metam erroru un tālāk nekas neizpildās
         this.validetDetalasDatus(veids, modelis, cena)
 
+        // pārrakstām esošās detaļu vērtības
         this.veids = veids
         this.modelis = modelis
         this.cena = cena
     }
 
+    // datu validācijs metode
     validetDetalasDatus (veids, modelis, cena) {
         if (!veids || !modelis || cena === undefined) {
             throw Error('Nav padota visa nepieciešamā informācija!')
@@ -31,19 +37,23 @@ class DatoraDetala {
 
 }
 
+// šādi tiek veidota jauna detaļa OOP pierakstā, kur mēs no klases izveidojam detaļas instanci
 const detala = new DatoraDetala('RAM', 'Corsair Vengeance LPX 16GB', 99.99)
 
-
-class DatoraDetalasForma {
+// Klase, kas atbild par to, lai forma strādātu kā vēlamies
+class DatoraDetalasFormas {
     constructor(addFormSelector) {
+        // izvēlamies formas elementu
         this.addFormElement = document.querySelector(addFormSelector)
 
+        // iedodam formas elementam eventa listeneri uz submit
         this.addFormElement.addEventListener('submit', (eventObject) => {
+            // sakam lai uz submita lapa nepārlādējās
             eventObject.preventDefault()
-
+            // iegūstam objektu, kurā ir pieejama indormācija par formas inputu vērtībām
             const formData = new FormData(this.addFormElement);
 
-
+            // šādi var iegūt vērtības, kur veids === input name
             console.log(formData.get('veids'))
             console.log(formData.get('modelis'))
             console.log(formData.get('cena'))
@@ -51,5 +61,5 @@ class DatoraDetalasForma {
     }
 }
 
-
-const datoruDetalasDarbibas = new DatoraDetalasForma('.js-add-form')
+// inicializējam klasi
+const datoruDetalasDarbibas = new DatoraDetalasFormas('.js-add-form')
