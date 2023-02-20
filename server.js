4bc971ae8c040f3ff0ef89710b9342beb4526f48
Serzsh <serzants.janis@gmail.com>
2023-02-20 15:20:59 +0200

    updates

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 })
 
+// mūsu loģika būs, ka ik reizi mēs ieejam url:
+// /create_txt?veids=RAM&modelis=Corsair%20Vengeance%20LPX%2016GB&cena=99.99
+// tiek paņemti dati no linka (query params) un izveidots txt fails
+app.get('/create_txt', (req, res) => {
+
+  // rīt turpinām izveidot jaunu txtx failu ar attiecīgo infu
+  console.log(req.query);
+
+  res.json('ok')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
