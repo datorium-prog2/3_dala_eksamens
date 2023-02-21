ae8a7c391f996c73b3ef95e9bc31d0ede7a698ea
Serzsh <serzants.janis@gmail.com>
2023-02-21 13:49:13 +0200

    updates

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const fs = require('fs');
+
 const app = express()
 const port = 3000
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(`${__dirname}/public/index.html`);
 })
 
 // mūsu loģika būs, ka ik reizi mēs ieejam url:
@@ -16,9 +18,22 @@ app.get('/create_txt', (req, res) => {
   // rīt turpinām izveidot jaunu txtx failu ar attiecīgo infu
   console.log(req.query);
 
-  res.json('ok')
+  // req.query = { veids: 'RAM', modelis: 'Corsair Vengeance LPX 16GB', cena: '99.99' }
+  // ram_corsair_vengeance_lpx_16gb.txt
+
+  const fileName = (`${req.query.veids}_${req.query.modelis}`).toLowerCase().replaceAll(" ", "_")
+  const fileContent = `-Personālā datora sastāvdaļa-
+Veids: ${req.query.veid}
+Modelis: ${req.query.modelis}
+Cena: ${req.query.cena} EUR
+`
+
+  fs.writeFileSync(`${__dirname}/public/txt/${fileName}.txt`, fileContent);
+
+  res.download(`${__dirname}/public/txt/${fileName}.txt`)
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
